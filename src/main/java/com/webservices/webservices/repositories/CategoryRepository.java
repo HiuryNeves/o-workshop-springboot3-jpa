42f91bd74c1cd02248f911594f2582bb8ffa25d8
Hiury Neves <hiurygamerpvp@gmail.com>
2024-01-04 21:57:10 -0300

    Category

---
+++ src/main/java/com/webservices/webservices/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.webservices.webservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservices.webservices.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
