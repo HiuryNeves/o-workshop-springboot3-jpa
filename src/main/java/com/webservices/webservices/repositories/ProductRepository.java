31c804b724359b29c59b9ce8dbc67b27fb47e6d6
Hiury Neves <hiurygamerpvp@gmail.com>
2024-01-05 15:27:26 -0300

    Product

---
+++ src/main/java/com/webservices/webservices/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.webservices.webservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservices.webservices.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
