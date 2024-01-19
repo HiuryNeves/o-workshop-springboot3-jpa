904847eb743752c0f6db89f8269f67402c8c60cd
Hiury Neves <hiurygamerpvp@gmail.com>
2024-01-19 20:40:03 -0300

    resolution bug

--- src/main/java/com/webservices/webservices/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/webservices/webservices/services/exceptions/ResourceNotFoundException.java
@@ -2,8 +2,3 @@ package com.webservices.webservices.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
-	public ResourceNotFoundException(Object id) {
-		super("Resource not found. Id " + id);
-	}
-}
